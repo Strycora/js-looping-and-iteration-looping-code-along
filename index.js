a1cc2d26c6ec94602153922a14a5e1672bc0ca8a
Vikram Chatterjee <strycora@gmail.com>
2021-02-10 14:43:27 -0700

    commit tree efa1960e45d6fdd03887ed7ebac5dfcb5eb80633

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+  for (let i=0; i< gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+  return gifts;
+}
+
+function writeCards(namesArray, event) {
+  let cardsArray = []
+  for (let i= 0; i < namesArray.length; i++ ){
+    cardsArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+  }
+  return cardsArray;
+}
+
+function countDown (number) {
+  while (number >= 0){
+    console.log(number--);
+  }
+}
